c0ec632c8ebb03dbd8b3d0eb5b078b1e44627e7c
QZBrainon <brainonqueiroz@gmail.com>
2024-01-13 12:32:00 -0300

    init: set up base structure and general error handler

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, _req, res, _next) => {
+  const { statusCode, message } = err;
+  return res.status(statusCode || 500).json({ message });
+};
+
+module.exports = {
+  errorHandler,
+};
