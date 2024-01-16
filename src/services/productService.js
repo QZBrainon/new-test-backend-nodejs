b774a97b7031b553377df585950ee18f403d4364
QZBrainon <brainonqueiroz@gmail.com>
2024-01-16 02:49:30 -0300

    fix: added joined queries and excluded _id and __v mongo default keys from response

--- src/services/productService.js
+++ src/services/productService.js
@@ -5,19 +5,25 @@ const createProduct = async (productData) => {
 };
 
 const getAllProducts = async () => {
-  return await productModel.find();
+  return await productModel
+    .find({}, "-_id -__v")
+    .populate("category", "-_id -__v");
 };
 
 const getProductById = async (id) => {
-  return await productModel.findById(id);
+  return await productModel
+    .findById(id, "-_id -__v")
+    .populate("category", "-_id -__v");
 };
 
 const updateProduct = async (id, productData) => {
-  return productModel.findByIdAndUpdate(
-    id,
-    { $set: productData },
-    { new: true }
-  );
+  return productModel
+    .findByIdAndUpdate(
+      id,
+      { $set: productData },
+      { new: true, projection: { _id: 0, __v: 0 }, lean: true }
+    )
+    .populate("category");
 };
 
 const deleteProduct = async (id) => {
